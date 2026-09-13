d0668e8b9c32cb243fa71872546dcb62f0b50875
yul-devx <yul.devx@gmail.com>
2026-09-13 17:37:38 +0900

    not solved: [Programmers] 완주하지 못한 선수

--- Programmers/Hash/완주하지_못한_선수.py
+++ Programmers/Hash/완주하지_못한_선수.py
@@ -1,9 +1,15 @@
 ## 해시
 ## 완주하지 못한 선수 
 
-def solution():
-    answer = 0
+def solution(participant, completion):
+    answer = ''
     return answer
 
 if __name__ == "__main__":
-    solution()
+    answer1 = solution(["leo", "kiki", "eden"], ["eden", "kiki"])
+    answer2 = solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"])
+    answer3 = solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+
+    print(answer1)  # "leo"
+    print(answer2)  # "vinko"
+    print(answer3)  # "mislav"
