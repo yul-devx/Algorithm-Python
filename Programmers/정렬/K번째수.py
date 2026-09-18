f740bf0e86627b5eb37ab40edaa6a8ec9280bd7b
yul-devx <yul.devx@gmail.com>
2026-09-18 23:38:57 +0900

    solved: [Programmers] K번째수

--- Programmers/정렬/K번째수.py
+++ Programmers/정렬/K번째수.py
@@ -2,15 +2,14 @@
 ## K번째수
 
 def solution(array, commands):
-
     answer = []
 
     for (i, j, k) in commands:
-        answer.append(sorted(array[i-1:j])[k]) 
+        answer.append(sorted(array[i-1:j])[k-1])
 
     return answer   
 
 if __name__ == "__main__":
     answer1 = solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]	)
 
-    print(solution(answer1))    # [5, 6, 3]
+    print(answer1)    # [5, 6, 3]
