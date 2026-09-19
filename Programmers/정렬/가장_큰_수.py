e8041577bf5c7ca718065ab05bc4649d49b88374
yul-devx <yul.devx@gmail.com>
2026-09-19 15:51:12 +0900

    not solved: [Programmers] 가장 큰 수

---
+++ Programmers/정렬/가장_큰_수.py
@@ -0,0 +1,13 @@
+# 정렬
+## 가장 큰 수
+
+def solution():
+    answer = []
+    return answer   
+
+if __name__ == "__main__":
+    answer1 = solution([6, 10, 2])
+    answer2 = solution([3, 30, 34, 5, 9])
+
+    print(answer1)    # "6210"
+    print(answer2)    # "9534330"
