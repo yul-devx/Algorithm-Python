aeb72c8c25f6fb0f846e5215e87440d46c801ab3
yul-devx <yul.devx@gmail.com>
2026-09-20 15:50:02 +0900

    refactor: [LeetCode] 0242. Valid Anagram

--- LeetCode/Hash/0242_Valid_Anagram.py
+++ LeetCode/Hash/0242_Valid_Anagram.py
@@ -2,8 +2,8 @@
 ## 0242. Valid Anagram
 
 ### 접근방법: 해시테이블
-### 시간복잡도: O(n)
-### 공간복잡도: O(n)
+### 시간복잡도: O(N)
+### 공간복잡도: O(N)
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -11,21 +11,17 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        anagram_set = set(s)
         anagram_dict = {}
 
         for sdx in s:
             anagram_dict[sdx] = anagram_dict.get(sdx, 0) + 1
 
         for tdx in t:
-            if tdx not in anagram_set:
+            if (tdx not in anagram_dict or anagram_dict[tdx] < 1):
                 return False
 
             anagram_dict[tdx] -= 1
 
-            if anagram_dict[tdx] < 0:
-                return False   
-        
         return True   
 
 if __name__ == "__main__":
